ea86e6f6b0841a1e134cbac9b593a17ed894b61f
Eugene Shilov <eugeneshilov@Eugenes-MacBook-Pro.local>
2023-06-21 12:09:46 +0600

    your commit message

--- toptechscore/settings.py
+++ toptechscore/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = "toptechscore.wsgi.application"
 
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config()
 }
 
 
